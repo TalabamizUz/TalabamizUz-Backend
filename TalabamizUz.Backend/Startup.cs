95db295c50d242a109923a58053bff533820dec0
Mukhammadkarim Tukhtaboyev <75162831+MrTukhtaboyev@users.noreply.github.com>
2021-11-13 22:15:04 +0500

    gerge

--- TalabamizUz.Backend/Startup.cs
+++ TalabamizUz.Backend/Startup.cs
@@ -51,7 +51,7 @@ namespace TalabamizUz.Backend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
