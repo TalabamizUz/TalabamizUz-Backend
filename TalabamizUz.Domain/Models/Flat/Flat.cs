70524fd38b36c3e5292c3ae376f20468959e0b99
Sherali Jurayev <uzbekistanzews@gmail.com>
2021-11-13 13:25:01 +0500

    Flat modes

---
+++ TalabamizUz.Domain/Models/Flat/Flat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalabamizUz.Domain.Enums;
+
+namespace TalabamizUz.Domain.Models.Flat
+{
+    [Table("flat")]
+    public class Flat
+    {
+        [Column("id")]
+        public int Id {  get; set; }  
+        
+        [Column("floor_number")]
+        public int FloorNumber {  get; set; }  
+        
+        [Column("room_count")]
+        public int RoomCount {  get; set; }
+
+        [Column("price")]
+        public decimal Price {  get; set; }
+
+        [Column("type")]
+        public FlatType Type { get; set; }
+
+        [Column("condition")]
+        public FlatCondition Condition { get; set; }
+
+        [Column("location")]
+        public string Location {  get; set; }
+
+        [Column("empty_place_count")]
+        public int EmptyPlaceCount {  get; set; }
+
+        [Column("view_count")]
+        public int ViewCount {  get; set; }
+
+        [Column("tags")]
+        public string Tags {  get; set; }
+
+        [Column("target_client")]
+        public TargetClientType TargetClient {  get; set; }
+    }
+}
---
+++ TalabamizUz.Domain/Models/Flat/FlatDetail.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalabamizUz.Domain.Models.Flat
+{
+    [Table("flat_detail")]
+    class FlatDetail
+    {
+        [Column("id")]
+        public int Id {  get; set; }
+
+        [Column("flat_id")]
+        public int FlatId {  get; set; }
+
+        [Column("flat_photos_path")]
+        public string FlatPhotosPath {  get; set; }
+
+        [Column("has_kitchen")]
+        public bool HasKitchen {  get; set;}
+
+        [Column("has_bath_room")]
+        public bool HasBathRoom {  get; set;}
+
+        [Column("has_wifi")]
+        public bool HasWifi {  get;}
+
+        [Column("has_elevator")]
+        public bool HasElevator {  get; set;}
+
+        [Column("bus")]
+        public string Bus {  get;}
+
+        [Column("landlord_message")]
+        public string LandlordMassage {  get; set; }
+    }
+}
